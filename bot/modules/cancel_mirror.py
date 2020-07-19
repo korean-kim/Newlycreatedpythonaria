460c49514f9ec9c84a9052faa49733d58c5f39ad
HappyBoy0509 <65845452+HappyBoy0509@users.noreply.github.com>
2020-07-19 12:42:04 +0530

    Update cancel_mirror.py

--- bot/modules/cancel_mirror.py
+++ bot/modules/cancel_mirror.py
@@ -64,9 +64,9 @@ def cancel_all(update, context):
     sendMessage(f'Cancelled {count} downloads!', context.bot,update)
 
 
-cancel_mirror_handler = CommandHandler(BotCommands.CancelMirror, cancel_mirror,
-                                       filters=CustomFilters.authorized_chat | CustomFilters.authorized_user)
-cancel_all_handler = CommandHandler(BotCommands.CancelAllCommand, cancel_all,
-                                    filters=CustomFilters.owner_filter)
+cancel_mirror_handler = CommandHandler(BotCommands.CancelMirror, cancel_mirror)
+                                     
+cancel_all_handler = CommandHandler(BotCommands.CancelAllCommand, cancel_all)
+                                   
 dispatcher.add_handler(cancel_all_handler)
 dispatcher.add_handler(cancel_mirror_handler)
