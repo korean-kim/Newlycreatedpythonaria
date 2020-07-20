2d8ee7bd018f658f0bf823f498e763061964d7a2
HappyBoy0509 <65845452+HappyBoy0509@users.noreply.github.com>
2020-07-20 21:10:54 +0530

    Update mirror.py

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -108,15 +108,15 @@ class MirrorListener(listeners.MirrorListeners):
 
     def onUploadComplete(self, link: str):
         with download_dict_lock:
-            msg = f'<b>Filename:</b> <code>{download_dict[self.uid].name()}</code>\n\n<b>Size:</b> <i>{download_dict[self.uid].size()}</i>\n\n<b>Gdrive:</b> {link}'            
+            msg = f'<b>Filename:</b> <code>{download_dict[self.uid].name()}</code>\n\n<b>Size:</b> <i>{download_dict[self.uid].size()}</i>\n\n<b>Gdrive Link🔗:</b> {link}'            
             LOGGER.info(f'Done Uploading {download_dict[self.uid].name()}')
             if INDEX_URL is not None:
                 share_url = requests.utils.requote_uri(f'{INDEX_URL}/{download_dict[self.uid].name()}')
                 if os.path.isdir(f'{DOWNLOAD_DIR}/{self.uid}/{download_dict[self.uid].name()}'):
                     share_url += '/'
-                msg += f'\n\n<b>Index:</b> {share_url}'
+                msg += f'\n\n<b>Shareable Link🔗:</b> {share_url}\n\nStream Link🖥:{share_url}?a=view'
             if self.tag is not None:
-                msg += f'\ncc: @{self.tag}'
+                msg += f'\n\ncc: @{self.tag}\n  #HB4All More @HB4All_Bot'
             try:
                 fs_utils.clean_download(download_dict[self.uid].path())
             except FileNotFoundError:
